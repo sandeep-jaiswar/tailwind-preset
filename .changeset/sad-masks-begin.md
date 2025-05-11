8f0c6f6b914d9d32d255b267e9fc80bc2497fe67
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-05-11 14:03:18 +0530

    chore: update changelog

---
+++ .changeset/sad-masks-begin.md
@@ -0,0 +1,5 @@
+---
+'@sandeep-jaiswar/tailwind-preset': patch
+---
+
+feat: add token in our preset v0.0.2
--- src/tokens/breakpoints.ts
+++ src/tokens/breakpoints.ts
@@ -1,8 +1,8 @@
 export const screens = {
-  'sm': '600px',
-  'md': '900px',
-  'lg': '1200px',
-  'xl': '1536px',
+  sm: '600px',
+  md: '900px',
+  lg: '1200px',
+  xl: '1536px',
 };
 
 export type ThemeScreens = typeof screens;
